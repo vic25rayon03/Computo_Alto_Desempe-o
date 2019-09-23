4eb66009934085051228bd7a48f88bd9dfb0c86d
Norma Elizabeth BV <36415416+NormaElizabethBV@users.noreply.github.com>
2019-09-23 12:31:00 -0500

    Add files via upload

---
+++ programa6.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<mpi.h> 
+
+int main(int argc, char *argv[])
+{
+	int dato,id,reduction;
+	MPI_Init(&argc,&argv); // Inicializa el ambiente
+	MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+	dato=id+1;
+
+	MPI_Reduce(&dato,&reduction,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+	
+	printf("Reduction de proceso %d es %d \n",id,reduction);
+	
+		
+	MPI_Finalize(); // Finaliza de ambiente
+	return 0;
+}
