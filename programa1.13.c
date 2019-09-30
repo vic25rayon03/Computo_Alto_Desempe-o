fb6d4a18d01a097bf29f9daff0c19c2a9c069503
Victor Eduardo Rayón Ochoa <42364302+vic25rayon03@users.noreply.github.com>
2019-09-30 12:48:07 -0500

    Update programa1.13.c

--- programa1.13.c
+++ programa1.13.c
@@ -4,6 +4,7 @@
 #include<math.h>
 #define p printf
 #define NPROCS 8
+
 int main(int argc,char *argv[])
 {
 	int rank,new_rank,sendbuf,recvbuf,numtasks, ranks1[4]={0,1,2,3},ranks2[4]={4,5,6,7};
@@ -12,6 +13,13 @@ int main(int argc,char *argv[])
 	MPI_Init(&argc,&argv); //Inicializa el ambiente
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+	if(numtasks!=NPROCS)
+	{
+		p("Se debe especificar MP_PROCS= %d . Terminando.\n", NPROCS);
+		return 0;
+	}
+
 //----------------------------------------------------------------------------------------------//
 	sendbuf=rank;
 	/*obtener el manejador de grupo original */
@@ -34,10 +42,6 @@ int main(int argc,char *argv[])
 	p("rank = %d newrank = %d recvbuf = %d\n",rank,new_rank,recvbuf);
 
 //----------------------------------------------------------------------------------------------//
-	if(numtasks!=NPROCS)
-	{
-		p("Se debe especificar MP_PROCS= %d . Terminando.\n", NPROCS);	
-	}
 	MPI_Finalize(); //Fin del ambiente
-	return 1;
+	return 0;
 }
