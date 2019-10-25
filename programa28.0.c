93362453eeb87ecbcaf4e29364a3540f4ab5dd54
Victor Eduardo Rayón Ochoa <42364302+vic25rayon03@users.noreply.github.com>
2019-10-25 13:18:30 -0500

    Producto Punto con cláusula de reducción OMP

---
+++ programa28.0.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
+
+#define n 100
+
+void llenaArreglo(double *a);
+
+double prodpunto(double *a,double *b);
+
+main(){
+double res,*a,*b ;
+
+a=(double *)malloc(n*sizeof(double));
+b=(double *)malloc(n*sizeof(double));
+
+llenaArreglo(a);
+llenaArreglo(b);
+
+
+res=prodpunto(a,b);
+printf("El resultado es %lf",res);
+
+
+}
+
+
+void llenaArreglo(double *a){
+  int i;
+    for(i=0;i<n;i++){
+    a[i]=1;
+
+    }
+
+
+}
+
+
+double prodpunto(double *a,double *b)
+{
+ 
+  double res=0;
+  int i;
+  #pragma omp paralell reduction(+:res)
+  //{
+	//#pragma omp for 
+	for(i=0;i<n;i++)
+	{
+		res+=a[i]*b[i];
+
+	}
+  //}
+return res;
+}
