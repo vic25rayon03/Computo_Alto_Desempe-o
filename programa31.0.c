739609a3c04b0a08463758f747611552708ee21a
Victor Eduardo Rayón Ochoa <42364302+vic25rayon03@users.noreply.github.com>
2019-10-28 13:30:01 -0600

    OMP Arreglo

---
+++ programa31.0.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
+#include<math.h>
+/*
+#pragma omp section
+{
+	sentencias
+}
+Se generan tiempos de espera al crear hilos, y se recomienda tener una sola region paralela
+*/
+
+main()
+{
+	int m[12][10],i,j,id;
+	
+	#pragma omp parallel private(j,id)
+	{
+		id=omp_get_thread_num();
+		#pragma omp for
+			for(i=0;i<12;i++)
+			{
+				for(j=0;j<10;j++)
+				{
+					m[i][j]=id;
+					printf("%d",m[i][j]);				
+				}
+			printf("\n");			
+			}	
+	}
+
+	return 0;
+}
+
