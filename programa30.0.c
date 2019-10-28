ad91b47db5ad9f16d6b81ed0510bf3c53ce744cd
Victor Eduardo Rayón Ochoa <42364302+vic25rayon03@users.noreply.github.com>
2019-10-28 13:17:14 -0600

    OMP Pi (Paralelo sin milisegundos)

---
+++ programa30.0.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+
+long long num_steps = 10000000;
+double step;
+
+int main(int argc, char* argv[])
+{
+	clock_t start, stop;
+	double x, pi, sum=0.0;
+	int i;
+	step = 1./(double)num_steps;
+	start = clock();
+	//Hilos
+	#pragma omp parallel for reduction(+:sum) private(x) //Región paralela (inicio) con constructores anidados
+		
+		//#pragma omp for reduction(+:sum) //Región paralela for
+		for (i=0; i<num_steps; i++)
+		{
+			x = (i + .5)*step;
+			sum = sum + 4.0/(1.+ x*x);
+		}
+	 //Fin región paralela
+	pi = sum*step;
+	stop = clock();
+
+	printf("El  valor de PI es %15.12f\n",pi);
+	printf("El tiempo para calcular PI fue %lf milisegundos\n",((double)(stop - start)/1000.0));
+	return 0;
+}
+
