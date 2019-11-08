e7cdd3390af52fd51bd109d03b2064cb8aa1dbb8
Victor Eduardo Rayón Ochoa <42364302+vic25rayon03@users.noreply.github.com>
2019-11-08 13:16:02 -0600

    OMP Task (no wait)

---
+++ programa34.1.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include <omp.h>
+#define p printf
+
+int main (int argc, char *argv[])
+{
+	#pragma omp parallel num_threads(4)
+	{
+		#pragma omp single
+		{
+			p("A ");
+			#pragma omp task			
+			{p("race ");}
+			#pragma omp task			
+			{p("car");}
+			p("Esperamos?");			
+		}	
+	}
+	p("\n");
+	return 0;
+}
