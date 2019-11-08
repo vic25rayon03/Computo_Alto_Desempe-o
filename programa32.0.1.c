5e9edc4019f9bbe2cc2cf168bab3e4a3f21f2d1c
Victor Eduardo Rayón Ochoa <42364302+vic25rayon03@users.noreply.github.com>
2019-11-08 12:49:49 -0600

    Pi OMP Paralelo

---
+++ programa32.0.1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+long long num_steps = 10000000;
+//long long num_steps = 10;
+double step;
+
+int main(int argc, char* argv[])
+{
+	//clock_t start, stop;
+	double inicio, fin, tmp;	
+	double x, pi, sum=0.0;
+	int i;
+	inicio=omp_get_wtime();
+	step = 1./(double)num_steps;
+	
+	//#pragma omp parallel for private(x,tmp)
+	#pragma omp parallel for private(x)		
+	//{
+		for (i=0; i<num_steps; i++)
+		{
+			x = (i + .5)*step;
+			//tmp= 4.0/(1.0+x*x);
+			#pragma omp critical	
+			//overhead paralelo		
+			sum = sum + 4.0/(1.+ x*x);
+			//sum+=tmp;		
+		}
+	
+		pi = sum*step;
+		//stop = clock();
+		fin=omp_get_wtime();
+//----------------------------------------------------------//
+	
+	//}	
+	printf("El  valor de PI es %15.12f\n",pi);
+	printf("El tiempo para calcular PI fue %lf milisegundos\n",(fin - inicio));
+	return 0;
+}
+
